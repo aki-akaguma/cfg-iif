7bec71e6fcaac4e990f668c419c1dfd087afde8d
aki <aki.akaguma@hotmail.com>
2018-06-13 16:44:36 +0900

    add no cfg keyword syntax.

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,34 @@ macro_rules! cfg_iif {
         }
     }};
     //
-    (#[cfg($($cfg_meta:meta)*)] {
+    (#[cfg($($cfg_meta:meta),*)] {
         $($it:tt)*
     }) => {{
-        #[cfg($($cfg_meta)*)]
+        #[cfg($($cfg_meta),*)]
+        {
+            $($it)*
+        }
+    }};
+    //
+    ($($cfg_meta:meta),* {
+        $($true_it:tt)*
+    } else {
+        $($false_it:tt)*
+    }) => {{
+        #[cfg($($cfg_meta),*)]
+        {
+            $($true_it)*
+        }
+        #[cfg(not($($cfg_meta),*))]
+        {
+            $($false_it)*
+        }
+    }};
+    //
+    ($($cfg_meta:meta),* {
+        $($it:tt)*
+    }) => {{
+        #[cfg($($cfg_meta),*)]
         {
             $($it)*
         }
@@ -29,9 +53,11 @@ macro_rules! cfg_iif {
 
 #[cfg(test)]
 mod tests {
+    #![allow(unused_mut)]
+    #![allow(unused_assignments)]
     //
     #[test]
-    fn it_works() {
+    fn it_works_01() {
         let a = {
             #[cfg(Unix)]
             {
@@ -56,6 +82,106 @@ mod tests {
             }
         };
         let a_iif = cfg_iif!(#[cfg(Unix)] { "windows" } else { "not windows" });
+        assert_eq!(a_iif, a);
+        //
+        let a = {
+            #[cfg(feature = "has_abc")]
+            {
+                "abc"
+            }
+            #[cfg(not(feature = "has_abc"))]
+            {
+                "not abc"
+            }
+        };
+        let a_iif = cfg_iif!(#[cfg(feature = "has_abc")] { "abc" } else { "not abc" });
+        assert_eq!(a_iif, a);
+        //
+        let mut a = "";
+        let mut a_iif = "";
+        #[cfg(feature = "has_abc")]
+        {
+            a = "abc";
+        };
+        cfg_iif!(#[cfg(feature = "has_abc")]
+        {
+            a_iif = "abc";
+        });
+        assert_eq!(a_iif, a);
+        //
+        let mut a = "";
+        let mut a_iif = "";
+        #[cfg(feature = "has_abc")]
+        {
+            a = "abc";
+        };
+        cfg_iif!(#[cfg(feature = "has_abc")]
+        {
+            a_iif = "abc";
+        });
+        assert_eq!(a_iif, a);
+    }
+    #[test]
+    fn it_works_02() {
+        let a = {
+            #[cfg(Unix)]
+            {
+                "unix"
+            }
+            #[cfg(not(Unix))]
+            {
+                "not unix"
+            }
+        };
+        let a_iif = cfg_iif!(Unix { "unix" } else { "not unix" });
+        assert_eq!(a_iif, a);
+        //
+        let a = {
+            #[cfg(Windows)]
+            {
+                "windows"
+            }
+            #[cfg(not(Windows))]
+            {
+                "not windows"
+            }
+        };
+        let a_iif = cfg_iif!(Unix { "windows" } else { "not windows" });
+        assert_eq!(a_iif, a);
+        //
+        let a = {
+            #[cfg(feature = "has_abc")]
+            {
+                "abc"
+            }
+            #[cfg(not(feature = "has_abc"))]
+            {
+                "not abc"
+            }
+        };
+        let a_iif = cfg_iif!(feature = "has_abc" { "abc" } else { "not abc" });
+        assert_eq!(a_iif, a);
+        //
+        let mut a = "";
+        let mut a_iif = "";
+        #[cfg(feature = "has_abc")]
+        {
+            a = "abc";
+        };
+        cfg_iif!(feature = "has_abc" {
+            a_iif = "abc";
+        });
+        assert_eq!(a_iif, a);
+        //
+        let mut a = "";
+        let mut a_iif = "";
+        #[cfg(feature = "has_abc")]
+        {
+            a = "abc";
+        };
+        cfg_iif!(feature = "has_abc" {
+            a_iif = "abc";
+        });
         assert_eq!(a_iif, a);
     }
 }
